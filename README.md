bd4ee5995f92133666dfef2ce79d9b9174fc6f66
Sowjanya Shetty <sowjanyashetty123456@gmail.com>
2024-04-18 06:19:35 +0530

    readme updated

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ Clone the repository from GitHub: git clone <repository-url>
 Install dependencies: pip install -r requirements.txt
 Set up MongoDB and configure connection settings in db_connection.py.
 Start the Flask application: python app.py
+
+
+# How to run this app using docker
+Make sure you have docker installed in your machine
+
+1 : Clone this repository
+    git clone https://github.com/sowjanyashetty12/weebhook-repo
+2 : cd TechStax_assessment
+3 : Start the Docker container
+    docker-compose up 
--- TechStax_assessment/app.py
+++ TechStax_assessment/app.py
@@ -29,8 +29,6 @@ def eventcheck():
   branch_name = data['ref'].split('/')[-1]
   time=data["head_commit"]["timestamp"]
   original_datetime = datetime.fromisoformat(time)
-
-
   formatted_date_time = original_datetime.strftime("%dth %B %Y - %I:%M%p")
   hash=data["head_commit"]["id"]
   eventdetails ={'request_id':hash,'author':author_name,'action':event_type,'from_branch':branch_name,'to_branch':branch_name,
@@ -51,18 +49,10 @@ def eventcheck():
   timestamp = data['pull_request']['created_at']
   pullrequestid=data['pull_request']['id']
   utc_time_str = timestamp
-
-#   utc_timezone = pytz.timezone('UTC')
-
   ist_timezone = pytz.timezone('Asia/Kolkata')
   utc_time = datetime.strptime(utc_time_str, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=pytz.utc)
-#   utc_time = datetime.strptime(utc_time_str, "%Y-%m-%dT%H:%M:%SZ")
-
   ist_time = utc_time.astimezone(ist_timezone)
-
   day_of_month = ist_time.strftime("%d")
-
-    # Determine the suffix for the day
   if day_of_month.endswith("1") and day_of_month != "11":
         suffix = "st"
   elif day_of_month.endswith("2") and day_of_month != "12":
@@ -71,15 +61,11 @@ def eventcheck():
         suffix = "rd"
   else:
         suffix = "th"
- 
-
-
   formatted_date_time = ist_time.strftime(f"%d{suffix} %B %Y - %I:%M%p")
   if  data["action"] == "closed" and data["pull_request"]["merged"]:
    event_type="merge"
    eventdetails ={'request_id':pullrequestid,'author':author,'action':event_type,'from_branch':from_branch,'to_branch':to_branch,
                   'time_stamp':formatted_date_time}
-#    print(event_type)
    store_event(eventdetails)
    app.logger.info(f"{author} merged branch {from_branch} to {to_branch} on {formatted_date_time}")
   else: 
@@ -93,7 +79,6 @@ def eventcheck():
 @app.route('/events', methods=['GET'])
 def get_latest_events():
  app.logger.info("Retrieving latest events")
- # Query  to get the latest events
  latest_events = list(collection.find().sort('time_stamp',-1).limit(5))
  for event in latest_events:
   event['_id'] = str(event['_id'])

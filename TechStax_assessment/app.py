05b7357bebe1fc65bef98b03b10fd70358027215
Sowjanya Shetty <sowjanyashetty123456@gmail.com>
2024-04-18 06:16:24 +0530

    Dockerized

--- TechStax_assessment/app.py
+++ TechStax_assessment/app.py
@@ -1,102 +1,103 @@
 
-from datetime import datetime, timezone, timedelta
-from flask import Flask, jsonify, request, json,render_template
+from datetime import datetime
+from flask import Flask,request, json,render_template
 from db_connection import store_event,collection
+import logging
+import pytz
 
 app=Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
 
 
 @app.route("/")
 def landingpage():
-    return render_template("index.html")
+ app.logger.info("Accessed landing page")
+ return render_template("index.html")
 
 
 @app.route("/webhook" ,methods=["POST"])
 def eventcheck():
-   print(request.headers)
-   event_type = request.headers.get('X-GitHub-Event')
+ app.logger.info("Received webhook request")
+ event_type = request.headers.get('X-GitHub-Event')
 
 
-   if event_type=="push":
-    jsondata=request.form.get('payload')
-    # print(jsondata)
-    data=json.loads(jsondata)
-    author_name=data["pusher"]["name"]
-    branch_name = data['ref'].split('/')[-1]
-    time=data["head_commit"]["timestamp"]
-    # print(type(time))
-    hash=data["head_commit"]["id"]
-    eventdetails ={'request_id':hash,'author':author_name,'action':event_type,'from_branch':branch_name,'to_branch':branch_name,
-                  'time_stamp':time}
-    store_event(eventdetails)
-    print(f"{author_name} pushed to {branch_name} on {time}")
+ if event_type=="push":
+  jsondata=request.form.get('payload')
+  data=json.loads(jsondata)
+  author_name=data["pusher"]["name"]
+  branch_name = data['ref'].split('/')[-1]
+  time=data["head_commit"]["timestamp"]
+  original_datetime = datetime.fromisoformat(time)
+
+
+  formatted_date_time = original_datetime.strftime("%dth %B %Y - %I:%M%p")
+  hash=data["head_commit"]["id"]
+  eventdetails ={'request_id':hash,'author':author_name,'action':event_type,'from_branch':branch_name,'to_branch':branch_name,
+                'time_stamp':formatted_date_time}
+  store_event(eventdetails)
+  app.logger.info(f"{author_name} pushed to {branch_name} on {formatted_date_time}")
 
 
     
-   elif event_type=="pull_request":
+ elif event_type=="pull_request":
     
-    jsondata=request.form.get('payload')
-    print(jsondata)
-    data=json.loads(jsondata)
-    author = data['pull_request']['user']['login']
-    from_branch = data['pull_request']['head']['ref']
-    to_branch = data['pull_request']['base']['ref']
-    timestamp = data['pull_request']['created_at']
-    # timestamp = data['pull_request']['updated_at']
-    
-    pullrequestid=data['pull_request']['id']
-         
-    utc_time_str = timestamp
+  jsondata=request.form.get('payload')
+  
+  data=json.loads(jsondata)
+  author = data['pull_request']['user']['login']
+  from_branch = data['pull_request']['head']['ref']
+  to_branch = data['pull_request']['base']['ref']
+  timestamp = data['pull_request']['created_at']
+  pullrequestid=data['pull_request']['id']
+  utc_time_str = timestamp
 
-    # Convert UTC string to datetime object
-    utc_time = datetime.fromisoformat(utc_time_str.replace("Z", "+05:30"))
+#   utc_timezone = pytz.timezone('UTC')
 
-    # Define IST timezone offset (+05:30)
-    ist_offset = timedelta(hours=5, minutes=30)
+  ist_timezone = pytz.timezone('Asia/Kolkata')
+  utc_time = datetime.strptime(utc_time_str, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=pytz.utc)
+#   utc_time = datetime.strptime(utc_time_str, "%Y-%m-%dT%H:%M:%SZ")
 
-    # Convert UTC time to IST time
-    ist_time = utc_time + ist_offset
+  ist_time = utc_time.astimezone(ist_timezone)
 
-    # Format IST time in the desired format
-    ist_time_str = ist_time.strftime("%Y-%m-%dT%H:%M:%S%Z")   
-    # print(ist_time_str) 
-    if  data["action"] == "closed" and data["pull_request"]["merged"]:
-     event_type="merge"
-     eventdetails ={'request_id':pullrequestid,'author':author,'action':event_type,'from_branch':from_branch,'to_branch':to_branch,
-                  'time_stamp':ist_time_str}
-     print(event_type)
-     store_event(eventdetails)
-     print(f"{author} merged branch {from_branch} to {to_branch} on {ist_time_str}")
-    else: 
-     eventdetails ={'request_id':pullrequestid,'author':author,'action':event_type,'from_branch':from_branch,'to_branch':to_branch,
-                  'time_stamp':ist_time_str}
-     store_event(eventdetails)
-     print(f"{author} submitted a pull request from {from_branch} to {to_branch} on {ist_time_str}")
-    
-#     # for merge
-#    elif event_type == "pull_request" and data["action"] == "closed" and data["pull_request"]["merged"]:
-#     author = data['pull_request']['user']['login']
-#     from_branch = data['pull_request']['head']['ref']
-#     to_branch = data['pull_request']['base']['ref']
-#     timestamp = data['pull_request']['merged_at']
-#     store_event(eventdetails)
-#     print(f"{author} merged branch {from_branch} to {to_branch} on {timestamp}")
-#     eventdetails ={'request_id':pullrequestid,'author':author,'action':event_type,'from_branch':from_branch,'to_branch':to_branch,
-#                   'time_stamp':timestamp}
-        
-    
-   return event_type
+  day_of_month = ist_time.strftime("%d")
+
+    # Determine the suffix for the day
+  if day_of_month.endswith("1") and day_of_month != "11":
+        suffix = "st"
+  elif day_of_month.endswith("2") and day_of_month != "12":
+        suffix = "nd"
+  elif day_of_month.endswith("3") and day_of_month != "13":
+        suffix = "rd"
+  else:
+        suffix = "th"
+ 
+
+
+  formatted_date_time = ist_time.strftime(f"%d{suffix} %B %Y - %I:%M%p")
+  if  data["action"] == "closed" and data["pull_request"]["merged"]:
+   event_type="merge"
+   eventdetails ={'request_id':pullrequestid,'author':author,'action':event_type,'from_branch':from_branch,'to_branch':to_branch,
+                  'time_stamp':formatted_date_time}
+#    print(event_type)
+   store_event(eventdetails)
+   app.logger.info(f"{author} merged branch {from_branch} to {to_branch} on {formatted_date_time}")
+  else: 
+   eventdetails ={'request_id':pullrequestid,'author':author,'action':event_type,'from_branch':from_branch,'to_branch':to_branch,
+                  'time_stamp':formatted_date_time}
+   store_event(eventdetails)
+   app.logger.info(f"{author} submitted a pull request from {from_branch} to {to_branch} on {formatted_date_time}")
+ return event_type
 
 
 @app.route('/events', methods=['GET'])
 def get_latest_events():
-    # Query MongoDB to get the latest events
-    latest_events = list(collection.find().sort('time_stamp',-1).limit(5))
-    # latest_events_sorted = sorted(latest_events, key=lambda x: datetime.strptime(x['time_stamp'], '%Y-%m-%dT%H:%M:%S%z'), reverse=True)
-    for event in latest_events:
-     event['_id'] = str(event['_id'])
-     
-    #  print(latest_events)
-    return json.dumps(latest_events)
+ app.logger.info("Retrieving latest events")
+ # Query  to get the latest events
+ latest_events = list(collection.find().sort('time_stamp',-1).limit(5))
+ for event in latest_events:
+  event['_id'] = str(event['_id'])
+ return json.dumps(latest_events)
 if __name__=="__main__":
-    app.run(debug=True)
+ app.run(host='0.0.0.0', port=5001)
+
--- TechStax_assessment/db_connection.py
+++ TechStax_assessment/db_connection.py
@@ -1,11 +1,23 @@
 from flask_pymongo import pymongo
-from datetime import datetime
-from bson.objectid import ObjectId
-from datetime import datetime
 
-client=pymongo.MongoClient("mongodb://localhost:27017/")
-db=client["github"]
-collection=db["events"]
+import logging
+
+# Initialize the logging configuration
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# client = pymongo.MongoClient('172.28.192.1', 27017)
+# client = pymongo.MongoClient(('mongodb://localhost:27017'))
+client = pymongo.MongoClient('mongodb://mongo:27017')
+db = client["github"]
+collection = db["events"]
+
 
 def store_event(eventdetails):
-  collection.insert_one(eventdetails)
+    try:
+        # Insert the event details into the collection
+        collection.insert_one(eventdetails)
+        logger.info("Event stored successfully")
+    except Exception as e:
+        # Log any exceptions that occur during the insertion process
+        logger.error(f"Error storing event: {e}")
